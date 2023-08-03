e341650ca5066ad18f9d7bc3684636777edc57b0
lehuutai0310 <lehuutai0310@gmail.com>
2023-08-03 17:41:33 +0700

    demo theory javascript

---
+++ src/comparisons/index.js
@@ -0,0 +1,12 @@
+// == and ===
+let a = 5;
+let b = "5";
+
+const isEqual = a == b; // true => compare to value
+const isStrictEqual = a === b; // => false,
+
+let c = 5;
+let d = "5";
+
+const isNotEqual = c != d; // false =>  compare to value <=> result true
+const isNotStrictEqual = c !== d; //  true => compare to data type
