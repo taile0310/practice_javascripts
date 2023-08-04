3d764729b27fb20ddcef658c16d7495b9ca2cf13
lehuutai0310 <lehuutai0310@gmail.com>
2023-08-04 09:06:12 +0700

    code demo practice comparisons

---
+++ src/comparisons/index.js
@@ -0,0 +1,22 @@
+// == and ===
+let a = 5;
+let b = '5';
+
+const isEqual = a == b; // true => compare to value
+const isStrictEqual = a === b; // => false,
+
+// != anad !==
+let c = 5;
+let d = '5';
+
+const isNotEqual = c != d; // false =>  compare to value <=> result true
+const isNotStrictEqual = c !== d; //  true => compare to data type
+
+// > < >= <=
+let num = 10;
+let num1 = 5;
+
+const isGreater = a > b; // => true
+const isLess = a < b; // => false
+const isGreaterOrEqual = a >= b; // => true,
+const isLessOrEqual = a <= b; // => false
