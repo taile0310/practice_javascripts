051b4509522b63cddea5ff6743e9862144b50345
lehuutai0310 <lehuutai0310@gmail.com>
2023-08-04 14:40:39 +0700

    code demo practice function

--- src/function/index.js
+++ src/function/index.js
@@ -16,3 +16,22 @@ function sum(num, num2) {
 }
 const result = sum(3, 5);
 console.log(result); // Output: 8
+
+// arrow function => not parameter
+const mess1 = () => {
+  console.log('Hello everybody, this is arrow function');
+};
+mess1();
+
+// arrow function with parameter
+const callMess1 = (message) => {
+  console.log(message);
+};
+callMess1('Hi everybody, this is arrow function');
+
+// arrow function with return
+const sum1 = (num3, num4) => {
+  return num3 + num4;
+};
+let result1 = sum(5, 5);
+console.log(result1);
