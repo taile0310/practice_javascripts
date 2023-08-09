4017fee274f196e25e9ad6e8c53040443c7d7852
lehuutai0310 <lehuutai0310@gmail.com>
2023-08-09 15:32:22 +0700

    update src code demo practice class

--- src/classes/index.js
+++ src/classes/index.js
@@ -1,10 +1,43 @@
 // Class
 class Person {
+  //constructor is special method used to initialize an object and is created in the class.
   constructor(name, age, city) {
     this.name = name;
     this.age = age;
     this.city = city;
   }
+
+  //setter is called through the assignment operator
+  setName(name) {
+    this.name = name;
+  }
+
+  setAge(age) {
+    this.age = age;
+  }
+
+  setCity(city) {
+    this.city = city;
+  }
+
+  //getter is called when accessing the property value.
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+
+  getCity() {
+    return this.city;
+  }
+
+  //Use "this" to refer to an object
+  hello() {
+    console.log(`My name is ${this.name}`);
+  }
+
   go(goTo) {
     console.log(goTo);
   }
@@ -29,5 +62,10 @@ class Customer extends Person {
   }
 }
 const customer = new Customer('Mai Nguyen', 25, 'Da Nang', 'Diamond', '0935310330');
+const customer1 = {};
 console.log(customer);
 customer.go('I go to play by car');
+
+//use "instanceof" check object type
+console.log(customer instanceof Customer);
+console.log(customer1 instanceof Customer);
