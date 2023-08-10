c0176df6475b728cd424e686a3dce227d352e093
lehuutai0310 <lehuutai0310@gmail.com>
2023-08-10 17:03:58 +0700

    branch main

--- src/exercise_2.3/index.js
+++
@@ -1,9 +0,0 @@
-const set_background = () => {
-  let paragraphs = document.getElementsByTagName('p');
-
-  for (let paragraph of paragraphs) {
-    paragraph.style.backgroundColor = 'pink';
-  }
-};
-const button = document.querySelector('input');
-button.onclick = set_background;
