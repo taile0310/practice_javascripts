5460253a01032f9d9b690bead8d285974a49f033
lehuutai0310 <lehuutai0310@gmail.com>
2023-08-21 20:11:08 +0700

    update src code exercise 2 after fix commoent of supporter

--- src/demo_form_login/controller/LoginController.js
+++ src/demo_form_login/controller/LoginController.js
@@ -3,39 +3,8 @@ class LoginController {
     this.model = model;
   }
 
-  setUsername(username) {
-    this.model.setUsername(username);
-  }
-
-  setPassword(password) {
-    this.model.setPassword(password);
-  }
-
-  login() {
-    const username = this.model.getUsername();
-    const password = this.model.getPassword();
-
-    // Perform input validation
-    if (username === '' || password === '') {
-      this.model.notify('Username or password cannot be empty', false);
-    } else {
-      // Perform authentication logic here
-      const users = [
-        { username: 'admin', password: 'admin' },
-        { username: 'admin1', password: 'admin1' },
-        { username: 'admin2', password: 'admin2' },
-        { username: 'admin3', password: 'admin3' },
-        { username: 'admin4', password: 'admin4' },
-      ];
-
-      const authenticatedUser = users.find((user) => user.username === username && user.password === password);
-
-      if (authenticatedUser) {
-        this.model.notify('Login successful.', true);
-      } else {
-        this.model.notify('Invalid username or password.', false);
-      }
-    }
+  login(username, password) {
+    this.model.authenticateUser(username, password);
   }
 
   setView(view) {
--- src/demo_form_login/view/LoginView.js
+++ src/demo_form_login/view/LoginView.js
@@ -4,27 +4,73 @@ class LoginView extends Observer {
   constructor(controller) {
     super();
     this.controller = controller;
+    this.controller.model.addObserver(this);
+
+    this.setupDOMElements();
+    this.setupEventListeners();
+    this.controller.setView(this);
+  }
+
+  //Set the required elements
+  setupDOMElements() {
     this.usernameInput = document.getElementById('username');
     this.passwordInput = document.getElementById('password');
     this.loginButton = document.getElementById('login-button');
     this.messageDiv = document.getElementById('message');
-    this.controller.model.addObserver(this);
-
-    this.usernameInput.addEventListener('input', (event) => {
-      this.controller.setUsername(event.target.value);
-    });
-
-    this.passwordInput.addEventListener('input', (event) => {
-      this.controller.setPassword(event.target.value);
-    });
-
-    this.loginButton.addEventListener('click', () => {
-      this.controller.login();
-    });
-
-    this.controller.setView(this);
   }
 
+  //Set the required events
+  setupEventListeners() {
+    //Username input check event
+    this.usernameInput.addEventListener('input', (event) => {
+      const username = event.target.value;
+      if (this.isValidEmail(username)) {
+        this.clearErrorMessage();
+      } else {
+        this.showMessage('Invalid email format', false);
+      }
+    });
+
+    //Password input check event
+    this.passwordInput.addEventListener('input', (event) => {
+      const password = event.target.value;
+      if (this.isValidPassword(password)) {
+        this.clearErrorMessage();
+      } else {
+        this.showMessage('Password must be at least 12 characters', false);
+      }
+    });
+
+    //Login event
+    this.loginButton.addEventListener('click', () => {
+      const username = this.usernameInput.value;
+      const password = this.passwordInput.value;
+
+      this.controller.login(username, password);
+    });
+  }
+
+  // Email regex
+  isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  // Password regex
+  isValidPassword(password) {
+    // At least 12 characters long but 14 or more is better
+    // A combination of uppercase letters, lowercase letters, numbers, and symbols
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?.&]{12,}$/;
+    return passwordRegex.test(password);
+  }
+
+  //Clear the error message
+  clearErrorMessage() {
+    this.messageDiv.textContent = '';
+    this.messageDiv.style.color = '';
+  }
+
+  //Show the message
   showMessage(message, isSuccess) {
     console.log(message);
     this.messageDiv.style.fontSize = '14px';
